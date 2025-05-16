0ae457176b452d0ae1cf0a4b50b676071fea9a30
Harish19-ar <harishegspec@gmail.com>
2025-05-16 11:53:10 +0530

    Simple Set Up Web project

---
+++ index.js
@@ -0,0 +1,36 @@
+setTimeout(() => { 
+document.getElementById("count").innerHTML=10 
+setTimeout(() => { 
+document.getElementById("count").innerHTML=9 
+setTimeout(() => {
+document.getElementById("count").innerHTML=8 
+setTimeout(() => { 
+document.getElementById("count").innerHTML=7 
+setTimeout(() => { 
+document.getElementById("count").innerHTML=6 
+setTimeout(() => { 
+document.getElementById("count").innerHTML=5 
+setTimeout(() => { 
+document.getElementById("count").innerHTML=4 
+setTimeout(() => { 
+document.getElementById("count").innerHTML=3 
+setTimeout(() => { 
+document.getElementById("count").innerHTML=2 
+setTimeout(() => { 
+document.getElementById("count").innerHTML=1 
+setTimeout(() => { 
+document.getElementById("count").innerHTML="Happy New Year" 
+console.log("Happy New Year") 
+},500) 
+},500) 
+},500) 
+},500)
+},500) 
+},500) 
+},500) 
+},500)
+},500) 
+},500) 
+},500)
+
+
